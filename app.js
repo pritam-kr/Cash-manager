b5322ca1bd03ce865c8487b0f96eebeab45e9367
Pritam Kumar <84632214+pritam-kr@users.noreply.github.com>
2021-07-21 12:26:51 +0530

    still working on it

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const form = document.querySelector('.form')
 const amountInputs = document.querySelectorAll('input')
-var myNotes = [];
+var myNotes = [2000, 500, 100, 20, 10, 5, 1];
+const noteList = document.querySelectorAll('.note')
 
 form.addEventListener('submit', (e) =>{
     e.preventDefault()
@@ -18,22 +19,39 @@ form.addEventListener('submit', (e) =>{
         document.querySelector('.output').innerHTML = blankMess;
     }else if(billValue > cashGiven){
         blankMess = `<div class="result">Given cash is less than bill, Please enter a valid ammount </div>`
-        document.querySelector('.output').innerHTML = blankMess;
-     }else if(cashGiven > billValue){
-        document.querySelector('.result').style.display = 'none';
-     }
+        document.querySelector('.output').innerHTML = blankMess;}
+    //  }else if(cashGiven > billValue){
+    //     document.querySelector('.result').style.display = 'none';
+    //  }
     
 })
 
 // count notes
 
 function calculateNotes(bill, cash){
-    var amountReturn = cash-bill;
+    let amountReturn = cash-bill;
 
-     console.log(amountReturn)
-    var mess = ''
+    //  console.log(amountReturn)
+
     if(amountReturn == 0){
         mess = `<div class="result"> No amount would be returned </div>`
         document.querySelector('.output').innerHTML = mess;
     }
+
+    for(i = 0; i < myNotes.length; i++){
+        amountReturns = compare(amountReturn, myNotes[i], i);
+    }
+}
+
+function compare(rtnAmt, arrayN, idx){
+   
+
+    if(rtnAmt >= arrayN){
+        let notes = Math.floor(rtnAmt/arrayN);
+        console.log(notes)
+        rtnAmt = rtnAmt - notes*arrayN;
+         noteList[idx].innerHTML = `${notes}`;
+        
+    }
+    return rtnAmt
 }
