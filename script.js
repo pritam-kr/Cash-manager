d958ed182f78787e537e9e2533a65f53db49d5b4
Pritam Kumar <84632214+pritam-kr@users.noreply.github.com>
2021-07-25 13:13:53 +0530

    change some styling

--- script.js
+++ script.js
@@ -2,34 +2,31 @@ const cashForm = document.querySelector(".form");
 const amountInputs = document.querySelectorAll("input");
 var myNotes = [2000, 500, 100, 20, 10, 5, 1];
 const noteList = document.querySelectorAll(".note");
-const btnNext = document.querySelector('.btn-next') ;
+const btnNext = document.querySelector(".btn-next");
 
-btnNext.addEventListener('click', (el) => {
-    el.preventDefault()
-    if(amountInputs[0].value>0){
-        document.querySelector('.cash-input').style.display = 'block';
-    }
-})
+btnNext.addEventListener("click", (el) => {
+  el.preventDefault();
+  if (amountInputs[0].value > 0) {
+    document.querySelector(".cash-input").style.display = "block";
+  }
+});
 
 cashForm.addEventListener("submit", (e) => {
   e.preventDefault();
- 
+
   var billValue = Number(amountInputs[0].value);
   var cashGiven = Number(amountInputs[1].value);
 
   if (billValue === 0 && cashGiven === 0) {
-        blankMess = `<div class="result">Please enter cash  </div>`;
-        document.querySelector(".output").innerHTML = blankMess;
-  }else if (billValue > cashGiven) {
-    blankMess = `<div class="result">Given cash is less than bill, Please enter a valid ammount </div>`
-    document.querySelector('.output').innerHTML = blankMess;
-    }else if(cashGiven > billValue) {
-        calculateNotes(billValue, cashGiven);
-        document.querySelector('.result').style.display = 'none'
-    }
-
-  
-  
+    blankMess = `<div class="result">Please enter cash  </div>`;
+    document.querySelector(".output").innerHTML = blankMess;
+  } else if (billValue > cashGiven) {
+    blankMess = `<div class="result">Given cash is less than bill, Please enter a valid ammount </div>`;
+    document.querySelector(".output").innerHTML = blankMess;
+  } else if (cashGiven > billValue) {
+    calculateNotes(billValue, cashGiven);
+    document.querySelector(".result").style.display = "none";
+  }
 });
 
 function calculateNotes(bill, cash) {
@@ -49,4 +46,3 @@ function compare(rtnAmt, arrayN, idx) {
   }
   return rtnAmt;
 }
-
